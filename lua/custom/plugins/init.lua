492c3c7e3286535fdcbb7f59c95f27d511e877ad
Luc Lapenta <lapentaluc@gmail.com>
2026-06-20 17:39:28 -0600

    add pretty-ts-errors plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -11,3 +11,16 @@ for file_name, type in vim.fs.dir(plugins_dir, { follow = true }) do
     require('custom.plugins.' .. module)
   end
 end
+
+vim.pack.add {
+  { src = 'https://github.com/youyoumu/pretty-ts-errors.nvim' },
+}
+
+require('pretty-ts-errors').setup {}
+
+-- Show error under cursor
+vim.keymap.set('n', '<leader>te', function() require('pretty-ts-errors').show_formatted_error() end, { desc = 'Show TS error' })
+-- Show all errors in file
+vim.keymap.set('n', '<leader>tE', function() require('pretty-ts-errors').open_all_errors() end, { desc = 'Show all TS errors' })
+-- Toggle auto-display
+vim.keymap.set('n', '<leader>tt', function() require('pretty-ts-errors').toggle_auto_open() end, { desc = 'Toggle TS error auto-display' })
